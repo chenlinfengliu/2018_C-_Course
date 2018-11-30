46fd233be669f4e1d1784bebf1ddaf5d9d342a63
陈林峰 <38061732+chenlinfengliu@users.noreply.github.com>
2018-11-30 11:33:31 +0800

    Timer_test

---
+++ c#/Timeconsole/Timeconsole/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Timeconsole
+{
+    partial class Time_console
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer_test = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer_test
+            // 
+            this.timer_test.Enabled = true;
+            this.timer_test.Interval = 10000;
+            this.timer_test.Tick += new System.EventHandler(this.timer_test_Tick);
+            // 
+            // Time_console
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "Time_console";
+            this.Text = "Timeconsole";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer_test;
+    }
+}
+
---
+++ c#/Timeconsole/Timeconsole/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timeconsole
+{
+    public partial class Time_console : Form
+    {
+        public Time_console()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_test_Tick(object sender, EventArgs e)
+        {
+            timer_test.Enabled = true;
+            MessageBox.Show("hello world");
+        }
+    }
+}
